e107755e7ef982d9aa8bd8cab4aa29ebeeb7d094
Shivam kumar <shivam68338@gmail.com>
2026-02-17 11:58:14 +0530

    Update GenerateImages.jsx

--- desi_ai/src/pages/GenerateImages.jsx
+++ desi_ai/src/pages/GenerateImages.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import { useAuth } from "@clerk/clerk-react";
 import toast from "react-hot-toast";
 
-const VITE_BASE_URL = "http://localhost:3000";
+const VITE_BASE_URL = "https://desi-ai-server1.onrender.com";
 
 const GenerateImages = () => {
   const imageStyle = [
